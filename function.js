a7082d8a1b54d080c4d9d346b6c5d2f3fc05bd81
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-13 22:22:21 +0600

    function

---
+++ function.js
@@ -0,0 +1,18 @@
+// no perameter
+const great = () => "hello";
+console.log(great());
+
+// single perameter
+const squre = x=> x * x;
+console.log(squre(5));
+
+// multiple perameter
+const add =(a,b) => a+b;
+console.log(add(5,5));
+
+// function body with multiple satatements
+const person = name => {
+    const greeting = "hello," + name + "!";
+    return greeting;
+}
+console.log(person("alice"));
