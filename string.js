618fab3e839cc871dc14f6314df53154d114c016
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-15 18:45:53 +0600

    if else

--- string.js
+++ string.js
@@ -1,15 +1,15 @@
-// combine strings in javascript
-// the operator.concatenation
-const str  = 'hello' +' '+'world';
-console.log(str);
-
-
-// array.join
-const mtr = ['hello', ' ' ,'world'].join('');
-console.log(mtr);
-
-
-// strng.concat
-const txt='hello';
-const res =txt.concat(' ','world');
-console.log(res);
+let number =45;
+if (number<=100 && 80<=number) {
+    console.log("your result is A+");
+}
+else if (number>=60 && number<80) {
+    console.log("your result is A");
+}else if (number<60 && number >= 50) {
+    console.log("your result is A-");
+}else if (number>= 40 && number<50 ) {
+    console.log("your result is B");
+}else if (number>=33 && number<40) {
+    console.log("your result is C");
+}else{
+    console.log("your resul is F");
+}
